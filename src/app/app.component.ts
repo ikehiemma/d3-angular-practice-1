4cb559ca426093f1869e97e146aaea4bdf8fe5a6
ikehiemma <ikehi_emma@yahoo.com>
2019-05-23 22:41:58 +0100

    box plot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,235 +14,146 @@ import { HttpClient } from '@angular/common/http';
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./app.component.css'],
   template: `
-    <div id="vizBody">
-      <div id="infobox">
-        <table>
-          <tr>
-            <th>Statistics</th>
-          </tr>
-          <tr>
-            <td>Team Name</td>
-            <td class="data"></td>
-          </tr>
-          <tr>
-            <td>Region</td>
-            <td class="data"></td>
-          </tr>
-          <tr>
-            <td>Wins</td>
-            <td class="data"></td>
-          </tr>
-          <tr>
-            <td>Losses</td>
-            <td class="data"></td>
-          </tr>
-          <tr>
-            <td>Draws</td>
-            <td class="data"></td>
-          </tr>
-          <tr>
-            <td>Points</td>
-            <td class="data"></td>
-          </tr>
-          <tr>
-            <td>Goals For</td>
-            <td class="data"></td>
-          </tr>
-          <tr>
-            <td>Goals Against</td>
-            <td class="data"></td>
-          </tr>
-          <tr>
-            <td>Clean Sheets</td>
-            <td class="data"></td>
-          </tr>
-          <tr>
-            <td>Yellow Cards</td>
-            <td class="data"></td>
-          </tr>
-          <tr>
-            <td>Red Cards</td>
-            <td class="data"></td>
-          </tr>
-        </table>
-      </div>
-      <div #vizContainer id="vizContainer"></div>
-      <div id="vizControls"></div>
-    </div>
+    <div #vizContainer id="vizContainer"></div>
   `
 })
 export class AppComponent implements OnInit {
   @ViewChild('vizContainer')
   private chartContainer: ElementRef;
 
+  scatterData = [
+    { friends: 5, salary: 22000 },
+    { friends: 3, salary: 18000 },
+    { friends: 10, salary: 88000 },
+    { friends: 0, salary: 180000 },
+    { friends: 27, salary: 56000 },
+    { friends: 8, salary: 74000 }
+  ];
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    d3.csv('../assets/data/worldcup.csv').then(data => this.dataViz(data));
-    /* this.http
-      .get('../assets/data/worldcup.csv', { responseType: 'text' })
-      .subscribe(data => {
-        var parsedData = d3.csvParse(data);
-        this.dataViz(parsedData);
-      }); */
+    d3.csv('../assets/data/boxplot.csv').then(data => this.dataViz(data));
+
+    //this.dataViz(this.scatterData);
   }
 
-  dataViz(incomingData) {
+  dataViz(data) {
     const element = this.chartContainer.nativeElement;
 
     d3.select(element)
       .append('svg')
       .attr(
         'style',
-        'height: 500px; width: 500px; border:1px lightgray solid;'
+        'height: 1000px; width: 1000px; border:1px lightgray solid;'
       );
 
+    //defining scales
+    const tickSize = 470;
+    const xScale = d3
+      .scaleLinear()
+      .domain([1, 8])
+      .range([20, tickSize]);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, 100])
+      .range([tickSize + 10, 20]);
+
+    //adding axis
+    const yAxis = d3
+      .axisRight()
+      .scale(yScale)
+      .ticks(8)
+      .tickSize(tickSize);
     d3.select('svg')
       .append('g')
-      .attr('id', 'teamsG')
-      .attr('transform', 'translate(50,300)')
-      .selectAll('g')
-      .data(incomingData)
+      //.attr('transform', `translate(${tickSize},0)`)
+      .attr('id', 'yAxisG')
+      .call(yAxis);
+
+    const xAxis = d3
+      .axisBottom()
+      .scale(xScale)
+      .tickSize(-tickSize)
+      .tickValues([1, 2, 3, 4, 5, 6, 7]);
+    d3.select('svg')
+      .append('g')
+      .attr('transform', `translate(0,${tickSize + 10})`)
+      .attr('id', 'xAxisG')
+      .call(xAxis);
+
+    d3.select('#xAxisG > path.domain').style('display', 'none');
+
+    //d3.selectAll('#xAxisG').attr('transform', 'translate(0,500)');
+
+    //scatter plot
+    d3.select('svg')
+      .selectAll('circle.median')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('class', 'tweets')
+      .attr('r', 5)
+      .attr('cx', d => xScale(d.day))
+      .attr('cy', d => yScale(d.median))
+      .style('fill', 'darkgray');
+
+    d3.select('svg')
+      .selectAll('g.box')
+      .data(data)
       .enter()
       .append('g')
-      .attr('class', 'overallG')
-      .attr('transform', (d, i) => 'translate(' + i * 50 + ', 0)');
-
-    var teamG = d3.selectAll('g.overallG');
-
-    teamG
-      .append('circle')
-      .attr('r', 0)
-      .transition()
-      .delay((d, i) => i * 100)
-      .duration(500)
-      .attr('r', 40)
-      .transition()
-      .duration(500)
-      .attr('r', 20);
-
-    teamG
-      .append('text')
-      .attr('y', 30)
-      .text(d => d.team);
-
-    const dataKeys = Object.keys(incomingData[0]).filter(
-      d => d !== 'team' && d !== 'region'
-    );
-
-    d3.select('#vizControls')
-      .selectAll('button.teams')
-      .data(dataKeys)
-      .enter()
-      .append('button')
-      .on('click', buttonClick)
-      .html(d => d);
-
-    function buttonClick(datapoint) {
-      var maxValue = d3.max(incomingData, d => parseFloat(d[datapoint]));
-
-      var colorQuantize = d3
-        .scaleQuantize()
-        .domain([0, maxValue])
-        .range(colorbrewer.Reds[3]);
-
-      var tenColorScale = d3
-        .scaleOrdinal()
-        .domain(['UEFA', 'CONMEBOL', 'CAF', 'AFC'])
-        .range(d3.schemeCategory10);
-
-      var ybRamp = d3
-        .scaleLinear()
-        .interpolate(d3.interpolateLab)
-        .domain([0, maxValue])
-        .range(['blue', 'yellow']);
-
-      var radiusScale = d3
-        .scaleLinear()
-        .domain([0, maxValue])
-        .range([2, 20]);
-
-      d3.selectAll('g.overallG')
-        .select('circle')
-        .transition()
-        .duration(1000)
-        .attr('r', d => radiusScale(d[datapoint]))
-        .style('fill', d => colorQuantize(d[datapoint]));
-    }
-
-    teamG.on('mouseover', highlightRegion);
-
-    function highlightRegion(d) {
-      var teamColor = d3.rgb('#75739F');
-      d3.select(this)
-        .select('text')
-        .classed('active', true)
-        .attr('y', 10);
-      d3.selectAll('g.overallG')
-        .select('circle')
-        .style('fill', p =>
-          p.region === d.region
-            ? teamColor.darker(0.75)
-            : teamColor.brighter(0.5)
-        );
-      this.parentElement.appendChild(this);
-    }
-
-    teamG.on('mouseout', unHighlight);
-    function unHighlight() {
-      d3.selectAll('g.overallG')
-        .select('circle')
-        .attr('class', '');
-      d3.selectAll('g.overallG')
-        .select('text')
-        .classed('active', false)
-        .attr('y', 30);
-    }
-
-    teamG.select('text').style('pointer-events', 'none');
-/* 
-    d3.selectAll('g.overallG')
-      .insert('image', 'text')
-      .attr('xlink:href', d => `../assets/images/${d.team}.png`)
-      .attr('width', '45px')
-      .attr('height', '20px')
-      .attr('x', -22)
-      .attr('y', -10); */
-
-    teamG.on('click', teamClick);
-    function teamClick(d) {
-      d3.selectAll('td.data')
-        .data(d3.values(d))
-        .html(p => p);
-    }
-
-    //d3.html("../assets/svg/noun_Football_1907.svg", data => {console.log(data)});
-    /* d3.xml('../assets/svg/noun_Football_1907.svg').then(data => {
-      loadSVG(data);
-      function loadSVG(svgData) {
-        d3.selectAll('g').each(function() {
-          var gParent = this;
-          d3.select(svgData)
-            .selectAll('path')
-            .each(function() {
-              gParent.appendChild(this.cloneNode(true));
-            });
-        });
-      }
-    });
-
-    d3.selectAll('g.overallG').each(function(d) {
-      d3.select(this)
-        .selectAll('path')
-        .datum(d);
-    });
-    var fourColorScale = d3
-      .scaleOrdinal()
-      .domain(['UEFA', 'CONMEBOL', 'CAF', 'AFC'])
-      .range(['#5eafc6', '#FE9922', '#93C464', '#fcbc34']);
-    d3.selectAll('path')
-      .style('fill', p => fourColorScale(p.region))
-      .style('stroke', 'black')
-      .style('stroke-width', '2px'); */
+      .attr('class', 'box')
+      .attr(
+        'transform',
+        d => 'translate(' + xScale(d.day) + ',' + yScale(d.median) + ')'
+      )
+      .each(function(d, i) {
+        d3.select(this)
+          .append('line')
+          .attr('class', 'range')
+          .attr('x1', 0)
+          .attr('x2', 0)
+          .attr('y1', yScale(d.max) - yScale(d.median))
+          .attr('y2', yScale(d.min) - yScale(d.median))
+          .style('stroke', 'black')
+          .style('stroke-width', '4px');
+        d3.select(this)
+          .append('line')
+          .attr('class', 'max')
+          .attr('x1', -10)
+          .attr('x2', 10)
+          .attr('y1', yScale(d.max) - yScale(d.median))
+          .attr('y2', yScale(d.max) - yScale(d.median))
+          .style('stroke', 'black')
+          .style('stroke-width', '4px');
+        d3.select(this)
+          .append('line')
+          .attr('class', 'min')
+          .attr('x1', -10)
+          .attr('x2', 10)
+          .attr('y1', yScale(d.min) - yScale(d.median))
+          .attr('y2', yScale(d.min) - yScale(d.median))
+          .style('stroke', 'black')
+          .style('stroke-width', '4px');
+        d3.select(this)
+          .append('rect')
+          .attr('class', 'range')
+          .attr('width', 20)
+          .attr('x', -10)
+          .attr('y', yScale(d.q3) - yScale(d.median))
+          .attr('height', yScale(d.q1) - yScale(d.q3))
+          .style('fill', 'white')
+          .style('stroke', 'black')
+          .style('stroke-width', '2px');
+        d3.select(this)
+          .append('line')
+          .attr('x1', -10)
+          .attr('x2', 10)
+          .attr('y1', 0)
+          .attr('y2', 0)
+          .style('stroke', 'darkgray')
+          .style('stroke-width', '4px');
+      });
   }
 }
